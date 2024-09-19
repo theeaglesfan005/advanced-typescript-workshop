89773349c9dd86c7c890681ab620db7f2ff67b06
Mark Thompson <mthompson@bitovi.com>
2024-09-19 12:50:44 -0700

    setup

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { Expect, Equal } from 'type-testing';
-
-const hello = "HELLO!!!"
-
-type test = Expect<Equal<typeof hello, 'HELLO!!!'>>;
-
-const hello2 = "moocow"
-
-type test2 = Expect<Equal<typeof hello2, 'HELLO!!!'>>;
