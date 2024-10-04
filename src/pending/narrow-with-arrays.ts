f86b8654961a43c4fd82372a11e2851048e28f7b
David Nicholas <dnicholas@Davids-MacBook-Pro.local>
2024-10-04 15:28:46 -0700

    update narrow-with-arrays

--- src/pending/narrow-with-arrays.ts
+++ src/pending/narrow-with-arrays.ts
@@ -1,6 +1,6 @@
 // source: https://github.com/total-typescript/advanced-patterns-workshop/blob/main/src/07-challenges/32-narrow-with-arrays.solution.ts
 
-// TODO: 
+// TODO:
 
 import { Equal, Expect } from "type-testing";
 
@@ -9,14 +9,28 @@ interface Fruit {
   price: number;
 }
 
-export const wrapFruit = <const TFruits extends readonly Fruit[]>(
-  fruits: TFruits,
-) => {
+type WrapFruit = (fruits: never) => {
+  getFruit: (name: never) => never;
+};
+
+declare var wrapFruit1: WrapFruit;
+
+const fruits1 = wrapFruit1([
+  {
+    name: "apple",
+    price: 1,
+  },
+  {
+    name: "banana",
+    price: 2,
+  },
+]);
+
+fruits1.getFruit("apple"); // should be {name: 'apple', price: 1}
+
+export const wrapFruit = <const TFruits extends readonly Fruit[]>(fruits: TFruits) => {
   const getFruit = <TName extends TFruits[number]["name"]>(name: TName) => {
-    return fruits.find((fruit) => fruit.name === name) as Extract<
-      TFruits[number],
-      { name: TName }
-    >;
+    return fruits.find((fruit) => fruit.name === name) as Extract<TFruits[number], { name: TName }>;
   };
 
   return {
@@ -42,5 +56,5 @@ const notAllowed = fruits.getFruit("not-allowed");
 
 type cases = [
   Expect<Equal<typeof apple, { readonly name: "apple"; readonly price: 1 }>>,
-  Expect<Equal<typeof banana, { readonly name: "banana"; readonly price: 2 }>>,
+  Expect<Equal<typeof banana, { readonly name: "banana"; readonly price: 2 }>>
 ];
