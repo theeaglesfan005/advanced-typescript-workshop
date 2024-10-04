22d78a0a552dc2507c3cee2e6449bba298356422
Mark Thompson <mthompson@bitovi.com>
2024-10-04 10:05:12 -0700

    utility types madness example

---
+++ src/exercises/utility-types-madness.ts
@@ -0,0 +1,28 @@
+// Easy
+
+// Utility Types, Pick, Omit
+
+/**
+ * Use 10 utility types to transform `Input` to `Output`.
+ * 
+ * Hint: You only need 1 utility type, so you'll have to get creative on how to include 9 other ones.
+ */
+
+import { Expect, Equal } from "type-testing";
+
+type Input = { hello: "world"; extra: "prop" };
+type Output = { hello: "world" };
+
+// This part doesn't need to be part of the solution
+// Just keeping this here to show what the "core" solution is
+type MainSolution<T extends Input> = Pick<T, "hello">;
+
+type MyType = Extract<
+    Exclude<
+        NoInfer<ReturnType<() => Parameters<(arg: NonNullable<Omit<Required<Partial<MainSolution<Input>>>, "">>) => void>[0]>>,
+        { moo: "cow" }
+    >,
+    Output
+>;
+
+type cases = [Expect<Equal<MyType, Output>>];
