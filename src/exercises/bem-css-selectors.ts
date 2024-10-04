8852d04eafd170b8d6aa1a6ae7e741dd1a64c4ec
Mark Thompson <mthompson@bitovi.com>
2024-10-04 12:10:17 -0700

    bem exercise

---
+++ src/exercises/bem-css-selectors.ts
@@ -0,0 +1,17 @@
+// source: https://github.com/type-challenges/type-challenges/blob/main/questions/03326-medium-bem-style-string/README.md
+
+// Easy
+
+// Template literals, Conditional Types
+
+import { Expect, Equal } from "type-testing";
+
+type BEM<B extends string, E extends string[], M extends string[]> = `${B}${E extends [] ? "" : `__${E[number]}`}${M extends []
+    ? ""
+    : `--${M[number]}`}`;
+
+type cases = [
+    Expect<Equal<BEM<"btn", ["price"], []>, "btn__price">>,
+    Expect<Equal<BEM<"btn", ["price"], ["warning", "success"]>, "btn__price--warning" | "btn__price--success">>,
+    Expect<Equal<BEM<"btn", [], ["small", "medium", "large"]>, "btn--small" | "btn--medium" | "btn--large">>,
+];
---
+++ src/exercises/omit-by-type.ts
@@ -0,0 +1,30 @@
+// source: https://github.com/type-challenges/type-challenges/blob/main/questions/02852-medium-omitbytype/README.md
+
+// Easy
+
+// Map Types, Conditional Types
+
+// Hint: https://www.typescriptlang.org/docs/handbook/2/mapped-types.html#key-remapping-via-as
+
+import { Expect, Equal } from "type-testing";
+
+// type OmitByType<T, U> = {
+//     [key in keyof T as T[key] extends U ? never : key]: T[key];
+// };
+
+type OmitByType<T, U> = {
+    [key in keyof T as T[key] extends U ? never : key]: T[key];
+};
+
+interface Model {
+    name: string;
+    count: number;
+    isReadonly: boolean;
+    isEnable: boolean;
+}
+
+type cases = [
+    Expect<Equal<OmitByType<Model, boolean>, { name: string; count: number }>>,
+    Expect<Equal<OmitByType<Model, string>, { count: number; isReadonly: boolean; isEnable: boolean }>>,
+    Expect<Equal<OmitByType<Model, number>, { name: string; isReadonly: boolean; isEnable: boolean }>>,
+];
