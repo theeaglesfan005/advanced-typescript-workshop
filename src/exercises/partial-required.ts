e8e64ef227bee5a1c5181a3cb30b266b4ae3d3db
Mark Thompson <mthompson@bitovi.com>
2024-10-04 11:14:37 -0700

    tuple to object exercise

--- src/exercises/partial-required.ts
+++ src/exercises/partial-required.ts
@@ -10,21 +10,11 @@ import { Prettify } from "../prettify";
 type PartialRequired<T, Keys extends keyof T> = Prettify<Required<Pick<T, Keys>> & Omit<T, Keys>>;
 // type PartialRequired<T, Keys extends keyof T> = Required<Pick<T, Keys>> & Omit<T, Keys>;
 
-type Test = PartialRequired<{ id?: string; userName?: string; password?: string }, 'id'>
-//   ^?
-
-const test = {} as Test;
-test.id;
-test.password;
-test.userName;
-
 type cases = [
     Expect<
     Equal<
-            PartialRequired<{ id?: string; userName?: string; password?: string, nullPromp: null, undefinedPromp: undefined }, "id" | 'userName'>,
-            { id: string; userName: string; password?: string, nullPromp: null, undefinedPromp: undefined }
+            PartialRequired<{ id?: string; userName?: string; password?: string }, "id" | 'userName'>,
+            { id: string; userName: string; password?: string }
         >
     >,
 ];
-
-
